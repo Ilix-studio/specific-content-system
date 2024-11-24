bab5d3c974db83c0bfcf2d1dcacc831903245426
ilix-studio <ilishjyoti17@gmail.com>
2024-11-25 00:48:12 +0530

    user-profile

--- apps/user-profile/src/user-profile.controller.ts
+++ apps/user-profile/src/user-profile.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+// src/user-profile.controller.ts
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { UserProfileService } from './user-profile.service';
+import { UserProfile } from './schema/user-profile.schema';
 
-@Controller()
+@Controller('user-profile')
 export class UserProfileController {
   constructor(private readonly userProfileService: UserProfileService) {}
 
+  @Post()
+  async createUserProfile(
+    @Body() body: Partial<UserProfile>,
+  ): Promise<UserProfile> {
+    return this.userProfileService.createUserProfile(body);
+  }
+
   @Get()
-  getHello(): string {
-    return this.userProfileService.getHello();
+  async getAllUserProfiles(): Promise<UserProfile[]> {
+    return this.userProfileService.getAllUserProfiles();
+  }
+
+  @Get(':id')
+  async getUserProfileById(@Param('id') id: string): Promise<UserProfile> {
+    return this.userProfileService.getUserProfileById(id);
   }
 }
